2b90d262299c581486a5823691cf319c26c8513a
sutanarif <39900329+sutanarif@users.noreply.github.com>
2021-01-04 16:02:49 +0700

    sutan

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/earsip';
+$config['base_url'] = 'http://localhost/earsip';
 
 /*
 |--------------------------------------------------------------------------
@@ -136,7 +136,8 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = "vendor/autoload.php";
+
 
 /*
 |--------------------------------------------------------------------------
