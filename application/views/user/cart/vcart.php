8424c2dec0fbf19b45825f37554943efa571a093
teukursy <43362156+teukursy@users.noreply.github.com>
2021-01-12 00:16:20 +0700

    vcart update add pelanggan

--- application/views/user/cart/vcart.php
+++ application/views/user/cart/vcart.php
@@ -8,16 +8,17 @@
 	<title>Arsip</title>
 
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	
+	<!-- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
+	
 
 </head>
 
 <body data-spy="scroll" data-target=".fixed-top">
 	<?php $this->load->view("user/_partials/navbar.php") ?>
-	<form action="<?php echo base_url('user/Cart/ubah_cart') ?>" method="post" name="frmShopping" id="frmShopping" class="form-horizontal" enctype="multipart/form-data">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<div class="container">
+		<?=$this->session->flashdata('notif')?>
 			<table id="cart" class="table table-hover table-condensed">
 				<thead>
 					<tr>
@@ -40,10 +41,10 @@
 						</td>
 						<td data-th="Price"> Rp. <?= $harga ?> </td>
 						<td data-th="Subtotal" class="text-center"> Rp. <?= $harga ?> </td>
-						<td class="actions" data-th="">
+					<!--	<td class="actions" data-th="">
 							<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
 							<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
-						</td>
+						</td> -->
 					</tr>
 				</tbody>
 				<tfoot>
@@ -51,15 +52,60 @@
 						<td class="text-center"><strong>Total Rp. <?= $harga ?></strong></td>
 					</tr>
 					<tr>
-						<td><a href="<?php echo site_url("Welcome/index") ?>" class="btn btn-warning"><i class="fa fa-angle-left"></i> Back To Order</a></td>
+						<td><a href="<?php echo site_url("") ?>" class="btn btn-warning"><i class="fa fa-angle-left"></i> Back To Order</a></td>
 						<td colspan="2" class="hidden-xs"></td>
 						<td class="hidden-xs text-center"><strong>Total Rp. <?= $harga ?></strong></td>
-						<td><button><a href="<?php echo site_url("user/Cart/print/" . $paket) ?>" class="btn btn-success" target="_blank">Checkout <i class="fa fa-angle-right"></i></a></button></td>
+						<td><button type="button" class="btn btn-info btn-md" data-toggle="modal" data-target="#tambah-data">Checkout</button></td>
+					
 					</tr>
 				</tfoot>
 			</table>
 		</div>
 
+		<!-- Modal Tambah -->
+		 <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="tambah-data" class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
+				<div class="modal-header">
+				<h4 class="modal-title">Tambah Data</h4>
+				</div>
+					<form class="form-horizontal" action="<?php echo site_url ("user/Cart/tambah") ?>" method="post" enctype="multipart/form-data" role="form">
+					<div class="modal-body">
+							<div class="form-group">
+								<label class="col-lg-2 col-sm-2 control-label">Nama</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" name="nama" placeholder="Tuliskan Nama">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-2 col-sm-2 control-label">Alamat</label>
+								<div class="col-lg-10">
+								<textarea class="form-control" name="alamat" placeholder="Tuliskan Alamat"></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-2 col-sm-2 control-label">Email</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" name="email" placeholder="Tuliskan Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-2 col-sm-2 control-label">Telp</label>
+								<div class="col-lg-10">
+									<input type="text" class="form-control" name="telp" placeholder="Tuliskan Telp">
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button class="btn btn-info" type="submit"> Simpan&nbsp;</button>
+							<button type="button" class="btn btn-warning" data-dismiss="modal"> Batal</button>
+						</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div> 
+		<!-- END Modal Tambah -->
 
 
 
@@ -67,8 +113,8 @@
 
 		<!-- Footer -->
 		<?php $this->load->view("user/_partials/footer.php") ?>
-
-
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 </body>
 
